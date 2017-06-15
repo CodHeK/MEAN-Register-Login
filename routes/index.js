8141641aa175df2a958f6dff0c2a60358c026993
CodHeK <gaganganapathyas@yahoo.in>
2017-06-15 15:40:17 +0530

    Added All files

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+//Get HomePage index.hbs
+router.get('/', ensureLoggedIn, function(req, res) {
+	res.render('index');
+});
+
+
+function ensureLoggedIn(req, res, next) {
+	if(req.isAuthenticated()) {
+		return next();
+	}
+	else {
+		req.flash('error_msg', 'You are not Logged in !');
+		res.redirect('/users/login');
+	}
+} 
+module.exports = router;
